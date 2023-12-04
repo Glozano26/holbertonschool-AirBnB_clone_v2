28c79f377a9f7f38971a38dde867a1f906c3e322
Glozano26 <lozsuperbia2014@gmail.com>
2023-12-04 11:12:13 -0500

    task 5 v1

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,16 +8,12 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    #def all(self):
-    """Returns a dictionary of models currently in storage"""
-        # return FileStorage.__objects
     def all(self, cls=None):
         """returns the list of objects of one type of class"""
-        if cls:
-            if isinstance(cls, str):
-                for obj in self.__objects.items():
-                    if isinstance(obj, cls):
-                        return FileStorage.__objects
+        if cls is not None:
+            return {key: obj for key, obj in self.__objects.items()
+                    if isinstance(obj, cls)}
+        return FileStorage.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -34,10 +30,10 @@ class FileStorage:
 
     def delete(self, obj=None):
         """delete objects"""
-        if obj in self.__objects:
-            del self.__objects[obj]
-        else:
-            pass
+        if obj is not None:
+            key = obj.__class__.__name__ + '.' + obj.id
+            if key in self.__objects:
+                del self.__objects[key]
 
     def reload(self):
         """Loads storage dictionary from file"""
