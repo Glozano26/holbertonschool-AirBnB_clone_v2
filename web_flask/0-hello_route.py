e6f255282f9b6d477425fbb547bcfa01df99ae5f
glozano26 <lozsuperbia2014@gmail.com>
2023-12-13 14:28:42 -0500

    task 0 v0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,26 +1,17 @@
 #!/usr/bin/python3
-'''
-MODULE NAME:
-------------
-    0-hello_route
+"""Write a script that starts a Flask web application:"""
 
-MODULE DESCRIPTION:
--------------------
-    That starts a Flask web application
-
-MODULE ATTRIBUTES:
-------------------
-    None
-'''
 from flask import Flask
+
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    
+    """Funtion that return a string"""
     return ("¡Hola HBNB!")
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
