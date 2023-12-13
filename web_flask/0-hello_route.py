21103cd8b27ab8cc8bba9742bd5f305b2372deab
glozano26 <lozsuperbia2014@gmail.com>
2023-12-13 14:51:15 -0500

    task 0 v1

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Funtion that return a string"""
-    return ("¡Hello HBNB!")
+    return ("Hello HBNB!")
 
 
 if __name__ == '__main__':
