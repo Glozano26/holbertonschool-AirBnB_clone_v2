7f00fe8827641e59db54deec4461abb8b1617c03
glozano26 <lozsuperbia2014@gmail.com>
2023-12-13 18:22:16 -0500

    task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -8,16 +8,20 @@ import urllib.parse
 app = Flask(__name__)
 
 
-@app.route('/c/<path:text>/python/<text>')
-def hello_hbnb(text, text2):
+@app.route('/c/<path:text>')
+def show_c(text):
     """replace underscore _ symbols with a space )"""
     decoded_text = urllib.parse.unquote_plus(text)
     text_notspace = decoded_text.replace('_', ' ')
-    
-    decoded_text2 = urllib.parse.unquote_plus(text2)
-    text_notspace2 = decoded_text.replace('_', ' ')
-    return (f'{text_notspace} {text_notspace2} is cool')
+    return (f'{text_notspace}')
+
+
+@app.route('/python/<path:text>')
+def show_python(text):
+    decoded_text = urllib.parse.unquote_plus(text)
+    text_notspace = decoded_text.replace('_', ' ')
+    return (f"Python {text_notspace}")
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True, strict_slashes=False)
