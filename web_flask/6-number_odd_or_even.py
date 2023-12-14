fa87e89d3cd34af51d5427ca3bd444176fddc12c
glozano26 <lozsuperbia2014@gmail.com>
2023-12-14 13:48:06 -0600

    task 6

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""Write a script that starts a Flask web application"""
+
+from flask import Flask
+from flask import render_template
+import urllib.parse
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def show_c(text):
+    """replace underscore _ symbols with a space )"""
+    decoded_text = urllib.parse.unquote_plus(text)
+    text_notspace = decoded_text.replace('_', ' ')
+    return (f'C {text_notspace}')
+
+
+@app.route('/python/')
+@app.route('/python/<text>', strict_slashes=False)
+def show_python(text="is cool"):
+    decoded_text = urllib.parse.unquote_plus(text)
+    text_notspace = decoded_text.replace('_', ' ')
+    return (f"Python {text_notspace}")
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def show_number(n):
+    return f'{n} is a number'
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_H1(n):
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<n>', strict_slashes=False)
+def even_and_odd(n):
+    if n % 2 == 0:
+        odd_even = n
+    else:
+        odd_even = n
+    return render_template('6-number_odd_or_even.html', odd_even=odd_even)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
