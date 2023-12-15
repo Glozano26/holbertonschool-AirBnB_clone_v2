bd496426fe5ccbdb421ea5187d18479c74bf909f
glozano26 <lozsuperbia2014@gmail.com>
2023-12-15 15:43:45 -0500

    task 9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -7,29 +7,27 @@ from models.state import State
 from models.city import City
 from operator import attrgetter
 
+
 app = Flask(__name__)
 
-@app.route('/states_list', strict_slashes=False)
-def sorted_state():
-    """List of all State objects present in DBStorage"""
-    states = storage.all(State).values()
-    states_sorted = sorted(states, key=attrgetter('name'))
-    return render_template('7-states_list.html', states=states_sorted)
 
 @app.route('/cities_by_states', strict_slashes=False)
 def sorted_cities_states():
     """List of all Cities objects present in State"""
     states = storage.all(State).values()
-    cities_sorted = []
-    for state in states:
+    states_sorted = sorted(states, key=attrgetter('name'))
+    cities_and_states = {}
+    for state in states_sorted:
         cities = sorted(state.cities, key=attrgetter('name'))
-        cities_sorted.extend(cities)
-    return render_template('8-cities_by_states.html', cities=cities_sorted)
+        cities_and_states[state] = cities
+    return render_template('8-cities_by_states.html', states_and_cities=cities_and_states)
+
 
 @app.teardown_appcontext
 def session_close(exception):
     """Close the current SQLAlchemy Session"""
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
