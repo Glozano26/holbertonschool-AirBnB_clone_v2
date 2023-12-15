14aa9e93ab4d25e1da6d8caa405d207777bce37a
glozano26 <lozsuperbia2014@gmail.com>
2023-12-15 14:32:31 -0500

    task 9

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Write a script that starts a Flask web application"""
+
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+from models.city import City
+from operator import attrgetter
+
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def sorted_state():
+    """list of all State objects present in DBStorage"""
+    states = storage.all(State).values()
+    states_sorted = sorted(states, key=attrgetter('name'))
+    return render_template('7-states_list.html', states=states_sorted)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def sorted_cities_states():
+    """list of all Cities objects present in State"""
+    cities = storage.all(State).values()
+    cities_sorted = []
+    for state in states:
+        cities = sorted(state.cities, key=attrgetter('name'))
+        cities_sorted.extend(cities)
+    return render_template('8-cities_by_states.html', cities=cities_sorted)
+
+
+@app.teardown_appcontext
+def session_close():
+    """close the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
