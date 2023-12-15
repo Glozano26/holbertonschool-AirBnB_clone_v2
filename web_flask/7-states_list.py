519c2cdc8bb2f671d4c871e26fe38f5b4c297415
glozano26 <lozsuperbia2014@gmail.com>
2023-12-15 12:42:01 -0500

    task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+"""Write a script that starts a Flask web application"""
+
+from flask import Flask
+from flask import render_template
+import urllib.parse
+from models import storage, State
+from operator import attrgetter
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def show_c(text):
+    """replace underscore _ symbols with a space )"""
+    decoded_text = urllib.parse.unquote_plus(text)
+    text_notspace = decoded_text.replace('_', ' ')
+    return (f'C {text_notspace}')
+
+
+@app.route('/python/')
+@app.route('/python/<text>', strict_slashes=False)
+def show_python(text="is cool"):
+    decoded_text = urllib.parse.unquote_plus(text)
+    text_notspace = decoded_text.replace('_', ' ')
+    return (f"Python {text_notspace}")
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def show_number(n):
+    return f'{n} is a number'
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_H1(n):
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def even_and_odd(n):
+    if n % 2 == 0:
+        total = 'even'
+    else:
+        total = 'odd'
+    return render_template('6-number_odd_or_even.html', n=n, total=total)
+
+
+@app.route('/states_list', strict_slashes=False)
+def sorted_state():
+    """list of all State objects present in DBStorage"""
+    states = storage.all(State).values()
+    states_sorted = sorted(states, key=attrgetter('name'))
+    return render_template('7-states_list.html', states=states_sorted)
+
+
+@app.teardown_appcontext
+def session_close(exception):
+    """close the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
